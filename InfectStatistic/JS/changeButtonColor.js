ec4ed95053fcc5deb00ab84ddbea36393470e335
Qi <154496613@qq.com>
2020-03-13 19:56:54 +0800

    添加按钮css变化

--- InfectStatistic/JS/changeButtonColor.js
+++ InfectStatistic/JS/changeButtonColor.js
@@ -1,4 +1,53 @@
-function changeColor()
+/*
+确定省份时，改变图表类型时按钮颜色变化
+*/
+
+function changeColor1()
 {
-    document.getElementById("ipButton").style.color = 'white';
+    document.getElementById("ipButton").style.backgroundColor= 'rgba(0, 191, 191, 1)';
+    document.getElementById("ipButton").style.color= 'white';
+    document.getElementById("cureButton").style.backgroundColor= 'white';
+    document.getElementById("cureButton").style.color= '#7f7f7f';
+    document.getElementById("deadButton").style.backgroundColor= 'white';
+    document.getElementById("deadButton").style.color= '#7f7f7f';
+    document.getElementById("allButton").style.backgroundColor= 'white';
+    document.getElementById("allButton").style.color= '#7f7f7f';
+   
+}
+
+function changeColor2()
+{
+    document.getElementById("cureButton").style.backgroundColor= 'rgba(0, 191, 191, 1)';
+    document.getElementById("cureButton").style.color= 'white';
+    document.getElementById("ipButton").style.backgroundColor= 'white';
+    document.getElementById("ipButton").style.color= '#7f7f7f';
+    document.getElementById("deadButton").style.backgroundColor= 'white';
+    document.getElementById("deadButton").style.color= '#7f7f7f';
+    document.getElementById("allButton").style.backgroundColor= 'white';
+    document.getElementById("allButton").style.color= '#7f7f7f';
+   
+}
+function changeColor3()
+{
+    document.getElementById("deadButton").style.backgroundColor= 'rgba(0, 191, 191, 1)';
+    document.getElementById("deadButton").style.color= 'white';
+    document.getElementById("cureButton").style.backgroundColor= 'white';
+    document.getElementById("cureButton").style.color= '#7f7f7f';
+    document.getElementById("ipButton").style.backgroundColor= 'white';
+    document.getElementById("ipButton").style.color= '#7f7f7f';
+    document.getElementById("allButton").style.backgroundColor= 'white';
+    document.getElementById("allButton").style.color= '#7f7f7f';
+   
+}
+function changeColor4()
+{
+    document.getElementById("allButton").style.backgroundColor= 'rgba(0, 191, 191, 1)';
+    document.getElementById("allButton").style.color= 'white';
+    document.getElementById("cureButton").style.backgroundColor= 'white';
+    document.getElementById("cureButton").style.color= '#7f7f7f';
+    document.getElementById("deadButton").style.backgroundColor= 'white';
+    document.getElementById("deadButton").style.color= '#7f7f7f';
+    document.getElementById("ipButton").style.backgroundColor= 'white';
+    document.getElementById("ipButton").style.color= '#7f7f7f';
+   
 }
